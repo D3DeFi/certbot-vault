6026806af8548a0771453acc5dc68809ad56df9e
Dusan Matejka <matejkadusan32@gmail.com>
2021-09-07 09:12:39 +0200

    more info to setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ setup(
     name='certbot-vault',
     version='0.1.0',
     description='Certbot plugin for interaction with HashiCorp Vault',
+    url='https://github.com/D3DeFi/certbot-vault',
     package='vault.py',
+    author='Dusan Matejka',
+    author_email='d3defi@gmail.com',
     install_requires=[
         'certbot',
         'hvac'
     ],
+    python_requires='>=3.6',
+    include_package_data=True,
     entry_points={
         'certbot.plugins': [
             'vault = vault:Installer',
